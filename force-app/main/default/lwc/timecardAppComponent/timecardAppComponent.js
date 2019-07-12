f6ce98ebdc0ee4c99ed24615c19825f65158d002
Adam Lincoln <alincoln@salesforce.com>
2019-07-12 12:12:12 -0400

    working on end user stuff

--- force-app/main/default/lwc/timecardAppComponent/timecardAppComponent.js
+++ force-app/main/default/lwc/timecardAppComponent/timecardAppComponent.js
@@ -1,19 +1,41 @@
 import { LightningElement, wire, track } from 'lwc';
 import getGrants from '@salesforce/apex/TimecardsAppController.getGrants';
+import getGrantWorkItems from '@salesforce/apex/TimecardsAppController.getGrantWorkItems';
+import saveTimecardsRecord from '@salesforce/apex/TimecardsAppController.saveTimecardsRecord';
 
 /** The delay used when debouncing event handlers before invoking Apex. */
 //const DELAY = 300;
 
 export default class TimecardAppComponent extends LightningElement {
-  @track contactId = '0032f000006xwONAAY';
+  @track contactId = new URLSearchParams(window.location.search).get('contactId');
+  @track grantId;
   @track grants;
+  @track grantWorkItems;
+  @track timecardRecord;
 
   @wire(getGrants, {contactId: "$contactId"}) wiredGrants({error, data}) {
+    console.log(this.contactId);
     if (data) {
       this.grants = data;
       console.log(this.grants);
     }
   }
+
+  handleSelectGrant(e) {
+    this.grantId = e.target.value;
+    getGrantWorkItems({"grantId": this.grantId}).then((response) => {
+        console.log(JSON.stringify(response));
+    });
+  }
+
+  /*@wire(getGrantWorkItems, {grantId: "$grantId"}) wiredGrantWorkItems({error, data}) {
+    if (data) {
+      this.grantWorkItems = data;
+      console.log(this.grantWorkItems);
+    }
+  }*/
+
+  @wire(saveTimecardsRecord, {jsonString: "$timecardRecord"}) wiredSaveTimecardsRecord({error, data}) {}
 
 /*
   handleKeyChange(event) {
